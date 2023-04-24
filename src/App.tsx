10d6ef326b87bc60d71aad0d42ca796468406ac9
JesperCaspeco <jesper.pettersson@caspeco.se>
2023-04-24 21:00:20 +0200

    First test

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import './App.css'
+import { Box } from './components/box'
+import { useState } from 'react'
 
-function App() {
+export const App = () => {
+  const [colorOne, setColorOne] = useState("blue");
   return (
     <>
       <h1>Praktikant test</h1>
+      <div className='main-container'>
+        <Box onClick={() => setColorOne("red")} color={colorOne} />
+      </div>
     </>
   )
 }
---
+++ src/components/box.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./box.css"
+
+interface IBoxProps {
+    color: string;
+    onClick?: () => void;
+}
+
+export const Box = ({ color, onClick }: IBoxProps) => {
+    return <div onClick={onClick} className="box" style={{ backgroundColor: color }}></div>
+}
